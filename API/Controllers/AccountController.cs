cf3f9aabc77fd2b8abd9669757eb2dc28e755b87
iliyalikomanov <iliyalikomanov@gmail.com>
2024-05-04 15:12:46 +0300

    Added boolean method "UserExists" in AccountController to check if the username exists in the Database. The method compares the username in lower letters to the database.The lower letters check is added, so there isn't doubleling of same letters. The "public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)" is also modified with the DTOs and it aslo adds usernames in the database in lower letters for the reason, menthion above.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -15,14 +17,16 @@ public class AccountController : BaseApiController
     }
 
     [HttpPost("register")] // POST: api/account/register
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+        if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
+
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.UserName.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -30,5 +34,10 @@ public class AccountController : BaseApiController
         await _context.SaveChangesAsync(); //adds the entity to the Database
 
         return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
 }
