a9beeb102997926f9d42745b974dbbdfe58d54a5
iliyalikomanov <iliyalikomanov@gmail.com>
2024-05-11 16:08:15 +0300

    Added NuGet "Microsoft.AspNetCore.Authentication.JwtBearer" UsersController - added [Authorize] atrubites at the level of the controller and [AllowAnonymous] for the "GetUsers" method. The [Autozie] atribute at that level requires authorization, the [AllowAnonymous] overrides it for the "GetUsers" method. Program.cs - Added 'builder.Services.AddAuthentication' for the token and 'app.UseAuthentication();' and 'app.UseAuthorization();'

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,12 +17,28 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding
+            .UTF8.GetBytes(builder.Configuration["TokenKey"])),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection();//remove later
 // Configure the HTTP request pipeline
 app.UseCors(builder => builder.AllowAnyHeader().WithOrigins("https://localhost:4200"));
+//Authentication midleware must be before 'app.MapControllers();' method and after 'app.UseCors()'.
+
+app.UseAuthentication(); //must be before 'app.UseAuthorization();'
+app.UseAuthorization();
 
 app.MapControllers();
 
