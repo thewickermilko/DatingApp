1c285d06fd87f9bdfe01561f16cf84f63a093602
iliyalikomanov <iliyalikomanov@gmail.com>
2024-05-12 21:45:25 +0300

    app.module.ts - FormsModule imported in "imports" array. This provides two-way binding between nav components and HTML tamplate. nav.components.ts - added property "model", added method "login". nav.component.html - "login" form is redone.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent
-  ],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
