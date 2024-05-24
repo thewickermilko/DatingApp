1a5c01b6347c70e59d3d38be9d86b7a98864d36b
iliyalikomanov <iliyalikomanov@gmail.com>
2024-05-24 17:23:51 +0300

    Added Database migration. Class "Photo" - added attribute "[Table("Photos")] - to overwrite the table name in the Database. Added properties "AppUserId" and "AppUser" - for relationship between "User" and "Photos" tables.

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,9 +1,18 @@
-﻿namespace API.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace API.Entities;
+
+[Table("Photos")]
+//overwrites the table name in the database - without it the name would be "Photo"
 public class Photo
 {
     public int Id { get; set; }
     public string Url { get; set; }
     public bool IsMain { get; set; }
     public string PublicId { get; set; }
+
+    //relationship property
+    //fully defined relationship
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
 }
