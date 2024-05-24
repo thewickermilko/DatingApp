2c0dbcd74ef24723cd3ef3d8e75194116f1e2cfa
iliyalikomanov <iliyalikomanov@gmail.com>
2024-05-24 16:55:13 +0300

    Created class "DateTimeExtensions" in "API/Extensions" folder. It has method "CalculateAge".

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Entities;
+﻿using API.Extensions;
+
+namespace API.Entities;
 
 public class AppUser
 {
@@ -17,4 +19,9 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new();
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
 }
---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Extensions;
+
+public static class DateTimeExtensions
+{
+    //this method is not accurate for leap year (29 of February), but for every other day - it's fine
+    public static int CalculateAge(this DateOnly dob)
+    {
+        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        var age = today.Year - dob.Year;
+
+        if (dob > today.AddYears(-age))
+        {
+            age--;
+        }
+
+        return age;
+    }
+}
