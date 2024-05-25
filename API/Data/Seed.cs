182727029493e603ff41e770d351e958bc382c24
iliyalikomanov <iliyalikomanov@gmail.com>
2024-05-25 09:58:20 +0300

    Created "UserSeedData.json" file with 5 random male and 5 random female users. Created class "Seed" in "API/Data" - for seeding users. Program.cs - Added login for entering the users in the database.

---
+++ API/Data/Seed.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class Seed
+{
+    public static async Task SeedUsers(DataContext context)
+    {
+        if (await context.Users.AnyAsync()) return;
+
+        var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);//check if you need "options" parameter
+
+        foreach (var user in users)
+        {
+            using var hmac = new HMACSHA512();
+
+            user.UserName = user.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+            //using complex password for the users. It's not unique, but that is for testing with Postman purpose and browser login.
+            user.PasswordSalt = hmac.Key;
+
+            context.Users.Add(user);
+        }
+
+        await context.SaveChangesAsync();
+    }
+}
