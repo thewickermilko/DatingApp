d64219189dd257b4dce55d63bfee9b5c269718fa
iliyalikomanov <iliyalikomanov@gmail.com>
2024-05-25 19:22:59 +0300

    Added Nuget "AutoMapper.Extensions.Microsoft.DependencyInjection". Creatred folder "Helpers". Created class "AutoMapperProfiles".

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace API.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<AppUser, MemberDto>();
+        CreateMap<Photo, PhotoDto>();
+    }
+}
