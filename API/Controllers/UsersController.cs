bb64656aa7f9af20d8a0d3b2b12b5938fff381d8
iliyalikomanov <iliyalikomanov@gmail.com>
2024-05-30 13:20:49 +0300

    Created "MemberUpdateDto" class. Added AutoMapperProfile for the class. "UsersController" class - added UpdateUser method.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using API.DTOs;
 using AutoMapper;
+using System.Security.Claims;
 
 namespace API.Controllers;
 
@@ -33,5 +34,22 @@ public class UsersController : BaseApiController
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         return await _userRepository.GetMemberAsync(username);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        //the question mark is to prevent having an "ArgumentNullException" if that username is null.
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return NotFound();
+
+        _mapper.Map(memberUpdateDto, user);//updates the properties, passed through the Dto,
+        //overwrites them in the "user" but nothing is still saved to the DataBase
+
+        if (await _userRepository.SaveAllAsync()) return NoContent(); //saves the changes in the DataBase
+
+        return BadRequest("Failed to update user");
     }
 }
