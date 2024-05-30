867351e0a1a6574350fd5a7d8b9e23360ff7b0ed
iliyalikomanov <iliyalikomanov@gmail.com>
2024-05-30 12:14:24 +0300

    Added "prevent-unsaved-changes.guard.ts". Added "HostListener" at "member-edit.component.ts".

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
@@ -14,6 +14,14 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm | undefined;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification(
+    $event: any
+  ) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  } //this activates the browser ability to prevent us from leaving where we were. If the form is dirty, it's going to open
+  //browser promp to ask if we want to contonue.
   member: Member | undefined;
   user: User | null = null;
 
