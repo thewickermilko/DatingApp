c26c06af495b956ddaa37feeff46641446da03bb
iliyalikomanov <iliyalikomanov@gmail.com>
2024-06-02 10:41:53 +0300

    Created "ClaimsPrincipalExtensions" class in API/Extensions. Class "UsersController" - added "AddPhoto" method.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using API.DTOs;
 using AutoMapper;
 using System.Security.Claims;
+using API.Extensions;
 
 namespace API.Controllers;
 
@@ -15,10 +16,12 @@ public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
+    private readonly IPhotoService _photoService;
 
-    public UsersController(IUserRepository userRepository, IMapper mapper)
+    public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
     {
         _mapper = mapper;
+        _photoService = photoService;
         _userRepository = userRepository;
     }
 
@@ -39,9 +42,7 @@ public class UsersController : BaseApiController
     [HttpPut]
     public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
     {
-        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        //the question mark is to prevent having an "ArgumentNullException" if that username is null.
-        var user = await _userRepository.GetUserByUsernameAsync(username);
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername(););
 
         if (user == null) return NotFound();
 
@@ -51,5 +52,31 @@ public class UsersController : BaseApiController
         if (await _userRepository.SaveAllAsync()) return NoContent(); //saves the changes in the DataBase
 
         return BadRequest("Failed to update user");
+    }
+
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+    {
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+        if (user == null) return NotFound();
+
+        var result = await _photoService.AddPhotoAsyncs(file);
+
+        if (result.Error != null) return BadRequest(result.Error.Message);
+
+        var photo = new Photo
+        {
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId
+        };
+
+        if (user.Photos.Count == 0) photo.IsMain = true;
+
+        user.Photos.Add(photo);
+
+        if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+
+        return BadRequest("Problem adding photo");
     }
 }
---
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace API.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string GetUsername(this ClaimsPrincipal user)
+    {
+        return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        //the question mark is to prevent having an "ArgumentNullException" if that username is null.
+    }
+}
