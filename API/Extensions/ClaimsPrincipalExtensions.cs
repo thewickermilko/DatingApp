c724e43ff684ad0e0118910260504f79ac645f3a
iliyalikomanov <iliyalikomanov@gmail.com>
2024-06-08 15:19:46 +0300

    'ClaimsPrincipalExtensions" class - "GetUsername" method updated. Created "GetUserId" method.

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -6,7 +6,12 @@ public static class ClaimsPrincipalExtensions
 {
     public static string GetUsername(this ClaimsPrincipal user)
     {
-        return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        return user.FindFirst(ClaimTypes.Name)?.Value;
         //the question mark is to prevent having an "ArgumentNullException" if that username is null.
+    }
+
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
     }
 }
