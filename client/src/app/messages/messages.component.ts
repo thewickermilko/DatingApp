603642099c1503e6c69293b631e614f76b40e660
iliyalikomanov <iliyalikomanov@gmail.com>
2024-06-13 06:33:06 +0300

    Created "message.service.ts"

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Message } from '../_models/messsage';
+import { Pagination } from '../_modules/pagination';
+import { MessageService } from '../_services/message.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  messages?: Message[];
+  pagination?: Pagination;
+  container = 'Outbox';
+  pageNumber = 1;
+  pageSize = 5;
 
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService
+      .getMessages(this.pageNumber, this.pageSize, this.container)
+      .subscribe({
+        next: (response) => {
+          this.messages = response.result;
+          this.pagination = response.pagination;
+        },
+      });
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
