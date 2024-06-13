3ef3284b7aba1fc55c25d3a8c61ce99714cc22d7
iliyalikomanov <iliyalikomanov@gmail.com>
2024-06-13 05:59:52 +0300

    "MessagesController" class - added "GetMessageThread" method.

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -4,6 +4,7 @@ using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
@@ -52,9 +53,34 @@ public class MessageRepository : IMessageRepository
             .CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
     }
 
-    public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
+    public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserName, string recipientUserName)
     {
-        throw new NotImplementedException();
+        var messages = await _context.Messages
+            .Include(u => u.Sender).ThenInclude(p => p.Photos)
+            .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+            .Where(
+                m => m.RecipientUsername == currentUserName &&
+                m.SenderUsername == recipientUserName ||
+                m.RecipientUsername == recipientUserName &&
+                m.SenderUsername == currentUserName
+            )
+            .OrderByDescending(m => m.MessageSent)
+            .ToListAsync();
+
+        var unreadMessages = messages.Where(m => m.DateRead == null
+            && m.RecipientUsername == currentUserName).ToList();
+
+        if (unreadMessages.Any())
+        {
+            foreach (var message in unreadMessages)
+            {
+                message.DateRead = DateTime.UtcNow;
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        return _mapper.Map<IEnumerable<MessageDto>>(messages);
     }
 
     public async Task<bool> SaveAllAsync()
