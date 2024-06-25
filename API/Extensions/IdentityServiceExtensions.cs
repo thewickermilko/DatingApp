77c64d91b82ebcb0aca8258c9afac16ca25f37da
iliyalikomanov <iliyalikomanov@gmail.com>
2024-06-25 15:00:56 +0300

    Created "AdminController" class. "IdentityServiceExtensions" class - code updated.

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -10,7 +10,7 @@ namespace API.Extensions;
 public static class IdentityServiceExtensions
 {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services,
-     IConfiguration config)
+        IConfiguration config)
     {
         services.AddIdentityCore<AppUser>(opt =>
         {
@@ -31,6 +31,12 @@ public static class IdentityServiceExtensions
                 ValidateIssuer = false,
                 ValidateAudience = false
             };
+        });
+
+        services.AddAuthorization(opt =>
+        {
+            opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+            opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
         });
 
         return services;
