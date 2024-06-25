57b7a5753897b2b3d86a301671da202f28cc60e1
iliyalikomanov <iliyalikomanov@gmail.com>
2024-06-25 10:33:21 +0300

    "SeedUsers" class - code updated to add "Roles".

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -9,7 +9,8 @@ namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(UserManager<AppUser> userManager)
+    public static async Task SeedUsers(UserManager<AppUser> userManager,
+     RoleManager<AppRole> roleManager)
     {
         if (await userManager.Users.AnyAsync()) return;
 
@@ -19,11 +20,32 @@ public class Seed
 
         var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);//check if you need "options" parameter
 
+        var roles = new List<AppRole>
+        {
+            new AppRole{Name = "Member"},
+            new AppRole{Name = "Admin"},
+            new AppRole{Name = "Moderator"},
+        };
+
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role); //creates the role
+        }
+
         foreach (var user in users)
         {
             user.UserName = user.UserName.ToLower();
 
             await userManager.CreateAsync(user, "Pa$$w0rd");
+            await userManager.AddToRoleAsync(user, "Member");
         }
+
+        var admin = new AppUser
+        {
+            UserName = "admin"
+        };
+
+        await userManager.CreateAsync(admin, "Pa$$w0rd");
+        await userManager.AddToRolesAsync(admin, new[] { "Admin", "Moderator" });
     }
 }
