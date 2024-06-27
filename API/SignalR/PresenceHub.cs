1c1312380a71f46e620d75d4e475233ff1d0f096
iliyalikomanov <iliyalikomanov@gmail.com>
2024-06-27 15:30:17 +0300

    Created "presence.service.ts".

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -7,7 +7,7 @@ public class PresenceHub : Hub
 {
     public override async Task OnConnectedAsync()
     {
-        await Clients.Others.SendAsync("UserIsOnlie", Context.User.GetUsername());
+        await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
