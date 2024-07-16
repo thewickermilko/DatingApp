390ef155de83d9434280b65984ae9985acbbf5bf
iliyalikomanov <iliyalikomanov@gmail.com>
2024-07-16 22:11:09 +0300

    Optimize queries for gender and messages.

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -83,9 +83,7 @@ public class MessageRepository : IMessageRepository
 
     public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserName, string recipientUserName)
     {
-        var messages = await _context.Messages
-            .Include(u => u.Sender).ThenInclude(p => p.Photos)
-            .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+        var query = _context.Messages
             .Where(
                 m => m.RecipientUsername == currentUserName && m.RecipientDeleted == false &&
                 m.SenderUsername == recipientUserName ||
@@ -93,9 +91,9 @@ public class MessageRepository : IMessageRepository
                 m.SenderUsername == currentUserName
             )
             .OrderBy(m => m.MessageSent)
-            .ToListAsync();
+            .AsQueryable();
 
-        var unreadMessages = messages.Where(m => m.DateRead == null
+        var unreadMessages = query.Where(m => m.DateRead == null
             && m.RecipientUsername == currentUserName).ToList();
 
         if (unreadMessages.Any())
@@ -106,7 +104,7 @@ public class MessageRepository : IMessageRepository
             }
         }
 
-        return _mapper.Map<IEnumerable<MessageDto>>(messages);
+        return await query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider).ToListAsync();
     }
 
     public void RemoveConnection(Connection connection)
