38a0fea2dd1d1d3b1938d97e19be5be91ffac361
iliyalikomanov <iliyalikomanov@gmail.com>
2024-07-23 05:43:47 +0300

    Created "FallbackController" class. Code updated.

--- API/Program.cs
+++ API/Program.cs
@@ -37,9 +37,13 @@ app.UseCors(builder => builder
 app.UseAuthentication(); //must be before 'app.UseAuthorization();'
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
